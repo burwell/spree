e67a867ce1aeda74401f53b403931417bde1c298
Burwell 2 <dayjp@dev-server.local>
2010-03-11 18:08:31 -0700

    a few changes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,3 +16,8 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Uncomment the line below if you want to use the gateway in test mode while deployed in production
+#ActiveMerchant::Billing::Base.gateway_mode = :test
+
+config.log_level = :debug
